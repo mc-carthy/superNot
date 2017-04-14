983ee52bd86a07b3ef375c554f9a7fdfddd0a119
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-04-14 10:23:42 +0100

    Implement basic movement

---
+++ Assets/scripts/PlayerInput.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class PlayerInput : MonoBehaviour {
+
+    private PlayerMovement pMove;
+	private Vector2 transInput;
+
+    private void Awake ()
+    {
+        pMove = GetComponent<PlayerMovement>();
+    }
+
+    private void Update ()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        transInput = new Vector2 (h, v).normalized;
+    }
+
+    private void FixedUpdate ()
+    {
+        pMove.Move(transInput);
+    }
+
+}
---
+++ Assets/scripts/PlayerMovement.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    private float moveSpeed = 5f;
+
+	public void Move(Vector2 transInput)
+    {
+        Vector3 transV3 = new Vector3(transInput.x, 0, transInput.y);
+        transform.Translate (transV3 * moveSpeed * Time.fixedDeltaTime);
+    }
+
+}
