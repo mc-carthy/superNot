9eda90e0b824168aaf70dc76195b87e762c6b5a8
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-05-15 21:28:24 +0100

    Create basic level and pathfinding guards

---
+++ Assets/scripts/EnemyPathfinding.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyPathfinding : MonoBehaviour {
+
+    private NavMeshAgent agent;
+    private Transform player;
+
+    private void Awake ()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+	private void Start()
+    {
+        agent.destination = player.position;
+    }
+
+}
