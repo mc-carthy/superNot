49bffa69bc8f4b113482f433286e46c65c46c7b3
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-05-17 10:30:05 +0100

    Enemies now spawn continuously

---
+++ Assets/scripts/EnemyManager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyManager : MonoBehaviour {
+
+    [SerializeField]
+    private float timeBetweenEnemySpawns;
+    [SerializeField]
+    private GameObject enemyPrefab;
+
+	private Vector3[] enemySpawnPoints;
+
+    private void Awake()
+    {
+        enemySpawnPoints = new Vector3[transform.childCount];
+
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            enemySpawnPoints[i] = transform.GetChild(i).transform.position;
+        }
+    }
+
+    private void Start()
+    {
+        StartCoroutine(SpawnEnemyRoutine());
+    }
+
+    private IEnumerator SpawnEnemyRoutine()
+    {
+        yield return new WaitForSeconds(timeBetweenEnemySpawns);
+        int spawnPointIndex = Random.Range(0, enemySpawnPoints.Length);
+
+        GameObject newEnemy = Instantiate(enemyPrefab, enemySpawnPoints[spawnPointIndex], Quaternion.identity);
+        newEnemy.transform.parent = transform;
+        
+        StartCoroutine(SpawnEnemyRoutine());
+    }
+
+}
